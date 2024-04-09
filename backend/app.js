032a987530e9db072fabd59b5b5196ecdfc78f34
Matias Harding <matias.harding@gmail.com>
2024-04-09 17:34:48 -0400

    Posts Schema, post save and find added

--- backend/app.js
+++ backend/app.js
@@ -38,47 +38,31 @@ app.use((req, res, next) => {
 })
 
 app.post('/api/posts',(req, res, next) => {
-  const post = req.body
-  console.log(post)
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content
+  })
+  post.save()
   res.status(201).json({
-    message: 'Success!'
+    message: 'Post added to database!'
   })
 })
 
 app.get('/api/posts',(req, res, next) => {
-  let postID = 1
-  const posts = [
-    {
-      "id": postID++,
-      "title": "Unforgettable Evening",
-      "content": "The ambiance was perfect for our anniversary dinner. The staff went above and beyond, making us feel special. Every dish was a delight, especially the seared scallops."
-    },
-    {
-      "id": postID++,
-      "title": "Family Friendly and Delicious",
-      "content": "Took the kids on Saturday and had a great time. The menu has something for everyone, and the kids' play area is a godsend. The pizza was a hit with the little ones!"
-    },
-    {
-      "id": postID++,
-      "title": "Best Brunch in Town",
-      "content": "Sunday brunch here is a must-try! The French toast with fresh berries is my go-to, and their coffee is simply the best. Cozy atmosphere and friendly service always."
-    },
-    {
-      "id": postID++,
-      "title": "Exquisite Vegetarian Options",
-      "content": "As a vegetarian, it's often hard to find a variety of options, but this place is a gem. The vegetarian lasagna was rich in flavor, and the garden salad was fresh and crisp."
-    },
-    {
-      "id": postID++,
-      "title": "A Disappointing Experience",
-      "content": "Unfortunately, our visit didn't meet expectations. The service was slow, and my steak was overcooked. However, the manager was understanding and offered a complimentary dessert."
-    }
-  ]
-  
-  res.status(200).json({
-    posts: posts,
-    message: 'Done!'
-  })
+
+  posts = Post.find()
+    .then(posts => {
+      res.status(200).json({
+        posts: posts,
+        message: 'Posts done!'
+      })
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(400).json({
+        error: err
+      })
+    })
 })
 
 module.exports = app
